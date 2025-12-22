f8d9208344c10e9cc6c1b9efda231904db36d1b0
Sofia <sofia.rios@qaracter.com>
2025-12-22 12:14:56 +0000

    Creado main para que funcionalidad basica

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        boolean repetir;
+
+        WarriorList wl = new WarriorList();
+        Warrior w = new Warrior("Sofia", 100, 10, 10, 5, diceRoll.d10);
+
+        wl.addWarrior(w);
+
+        System.out.println("Welcome to Battle Game Simulator!!\n");
+
+        do{
+            repetir = Menu.menu(sc, wl);
+        }while (repetir);
+
+        sc.close();
     }
 }
