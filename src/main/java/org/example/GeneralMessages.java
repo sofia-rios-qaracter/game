e03f4c810a29897ee51d8da9c4b3bd11b2613aed
Sofía <soceriba@gmail.com>
2025-12-22 20:11:49 +0100

    Creacion de mensajes generales

---
+++ src/main/java/org/example/GeneralMessages.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class GeneralMessages {
+    public static final String MUST_INT = "You must type a number";
+    public static final String PRESS_ENTER = "Please, press enter to continue";
+}
