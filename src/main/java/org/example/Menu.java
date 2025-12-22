c90bca512ac99cf39420ce1bc165a227d4b2a95f
Sofía <soceriba@gmail.com>
2025-12-22 18:02:54 +0100

    Extraido menu de combate por claridad
    Se ha extraido el menu de combate y se ha pasado a su propio archivo,
    ademas se han añadido readers para que el usuario pueda ir viendo que ha
    pasado a lo largo del combate

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -1,9 +1,10 @@
 package org.example;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
-    public static boolean menu(Scanner sc, WarriorList warriors){
+    public static boolean menu(Scanner sc, WarriorList warriors) {
         int menuOption;
         boolean repeat = true;
         Warrior warrior;
@@ -11,24 +12,29 @@ public class Menu {
         System.out.println("Do you have a warrior? Press 1 to load it.");
         System.out.println("Do you want to create a new warrior? Press 2 to load it.");
         System.out.println("Do you want to see all selected warrior? Press 3 to see them!");
-        System.out.println("Let's battle!! Press 5 to fight!!");
-        System.out.println("If you want to leave, please press 4");
+        System.out.println("Let's battle!! Press 4 to fight!!");
+        System.out.println("If you want to leave, please press 5");
         menuOption = Util.readInt(sc, "Choose your option!", "You must type a number");
 
-        switch (menuOption){
+        switch (menuOption) {
             case 1:
                 warrior = loadWarrior(sc);
-                if(warrior != null) warriors.addWarrior(warrior);
+                if (warrior != null)
+                    warriors.addWarrior(warrior);
+                Util.readWord(sc, "Press enter to coninue", "");
                 break;
             case 2:
                 warrior = createWarrior(sc);
-                if(warrior != null) warriors.addWarrior(warrior);
+                if (warrior != null)
+                    warriors.addWarrior(warrior);
+                Util.readWord(sc, "Press enter to coninue", "");
                 break;
             case 3:
                 System.out.println(warriors);
+                Util.readWord(sc, "Press enter to coninue", "");
                 break;
             case 4:
-                combatMenu(sc, warriors);
+                Combat.combatMenu(sc, warriors);
                 break;
             case 5:
                 repeat = false;
@@ -36,26 +42,19 @@ public class Menu {
                 break;
             default:
                 System.out.println("You have to type some of the posible options!!");
+                Util.readWord(sc, "Press enter to coninue", "");
                 break;
         }
         return repeat;
     }
 
-    public static Warrior loadWarrior(Scanner sc){
+    public static Warrior loadWarrior(Scanner sc) {
         System.out.println("Not implemented yet");
         return null;
     }
 
-    public static Warrior createWarrior(Scanner sc){
+    public static Warrior createWarrior(Scanner sc) {
         System.out.println("Not implemented yet");
         return null;
-    }
-
-    public static void combatMenu(Scanner sc, WarriorList warriors){
-        if(!warriors.canBattle()){
-            System.out.println("You must add more warriors!\nAt least are needed 2 warriors to fight.\n");
-            return;
-        }
-
     }
 }
