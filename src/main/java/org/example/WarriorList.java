d65b81e00ca4edd794af6b7e7a1a1fb91902eda3
Sofia <sofia.rios@qaracter.com>
2025-12-22 11:47:25 +0000

    Creada la clase lista de guerreros

--- src/main/java/org/example/WarriorList.java
+++ src/main/java/org/example/WarriorList.java
@@ -1,4 +1,21 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class WarriorList {
+    private HashMap<String, Warrior> warriors;
+
+    public WarriorList(){
+        this.warriors = new HashMap<>();
+    }
+
+    public boolean addWarrior(Warrior warrior){
+        if(this.warriors.containsKey(warrior.getName())) return false;
+
+        this.warriors.put(warrior.getName(), warrior);
+        return true;
+    }
+    public Warrior getWarrior(String name){
+        return this.warriors.get(name);
+    }
 }
