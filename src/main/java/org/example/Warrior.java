7c84be877a939f36a2259eeed9242be08d6c317b
Sofia <sofia.rios@qaracter.com>
2025-12-22 11:46:54 +0000

    Creada la clase guerrero

--- src/main/java/org/example/Warrior.java
+++ src/main/java/org/example/Warrior.java
@@ -1,4 +1,44 @@
 package org.example;
 
 public class Warrior {
+    private String name;
+    private int healthPoints;
+    private int defense;
+    private int attack;
+    private int speed;
+    private diceRoll damage;
+
+
+    public Warrior(String name, int healthPoints, int defense, int attack, int speed, diceRoll damage){
+        this.name = name;
+        this.healthPoints = healthPoints;
+        this.defense = defense;
+        this.attack = attack;
+        this.speed = speed;
+        this.damage = damage;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public boolean isDead(){
+        return this.healthPoints < 0;
+    }
 }
